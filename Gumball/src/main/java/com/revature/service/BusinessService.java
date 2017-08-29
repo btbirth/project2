fadfd80e4637c6d78158030495bdeaaab3fae99a
Brett <brett.birth@gmail.com>
2017-08-29 16:40:32 -0400

    bug fix

--- Gumball/src/main/java/com/revature/service/BusinessService.java
+++ Gumball/src/main/java/com/revature/service/BusinessService.java
@@ -3,14 +3,14 @@ package com.revature.service;
 
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
-
+import org.springframework.context.ApplicationContextAware;
 
 import com.revature.beans.Author;
 import com.revature.beans.Reader;
 import com.revature.daos.AuthorDAO;
 import com.revature.daos.ReaderDAO;
 
-public class BusinessService {
+public class BusinessService implements ApplicationContextAware {
 	private ApplicationContext context;
 	public void setApplicationContext(ApplicationContext context) throws BeansException {
 		this.context = context;
