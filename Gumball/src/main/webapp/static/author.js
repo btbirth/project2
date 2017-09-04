4c0bfa7c2d0327cd237a1e73f5b099ebd5e799c8
Omokorede <koredeallen@gmail.com>
2017-09-04 18:27:07 -0400

    author functionality

--- Gumball/src/main/webapp/static/author.js
+++ Gumball/src/main/webapp/static/author.js
@@ -1,17 +1,16 @@
 angular.module("magazineAPI",["ngRoute"]);
 angular.module("magazineAPI").controller("createCtrl", function($scope, $http){
 	$scope.createArticle  = function(){
-		console.log($scope.createArticle);
 		$http({
 				method:"POST",
 				url: "/Gumball/Article/create",
-				data:$scope.createArticle
+				data:$scope.newArticle
 		}).then(function(response){
 			console.log(response.status);
 		})
 	}
 })
-angular.module("magazineAPI").controller("myArtilcesCtrl",
+angular.module("magazineAPI").controller("myArticlesCtrl",
 		function($scope, $http){
 			$http({
 				method: "GET",
@@ -31,10 +30,7 @@ angular.module("magazineAPI").config(function($routeProvider, $locationProvider)
 		templateUrl : "NewArticle.html",
 		controller : "createCtrl"
 	}).when("/myArticles", {
-		templateUrl : "NewArticle.html",
-		controller : "createCtrl"
-	}).otherwise({
-		templateUrl : "dashone.html",
-		controller : "createCtrl",
+		templateUrl : "dashthree.html",
+		controller : "myArticlesCtrl"
 	});
 })
